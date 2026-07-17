87272eed32a5df7ed38dbf55bd3f51cca9c7169d
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-17 15:27:21 +0300

    feat(db): add fact_weather table to public schema with foreign keys

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -13,3 +13,25 @@ CREATE TABLE dim_weather_condition(
 	description VARCHAR(50),
 	icon VARCHAR(15)
 );
+
+CREATE TABLE fact_weather(
+	record_weather_id SERIAL PRIMARY KEY,
+	city_id INT REFERENCES dim_city(city_id),
+	weather_id INT REFERENCES dim_weather_condition(weather_id),
+	record_date TIMESTAMP,
+	temperature DECIMAL(5,2),
+	temp_feels_like DECIMAL(5,2),
+	temp_min DECIMAL(5,2),
+	temp_max DECIMAl(5,2),
+	pressure INT,
+	humidity INT,
+	pressure_sea_level INT,
+	pressure_ground_level INT,
+	wind_speed DECIMAL(5,2),
+	wind_direction_deg INT,
+	wind_gust DECIMAL(5,2),
+	sunrise TIMESTAMP,
+	sunset TIMESTAMP,
+	visibility INT,
+	clouds_percentage INT
+);
