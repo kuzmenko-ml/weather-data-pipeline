c80b4ea3f5583c1c8c0a15745ee36902e8de6798
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-19 15:09:08 +0300

    feat(TL pipeline): add get_raw_data to fetch new raw data based on last processed timestamp

--- src/TL_pipeline.py
+++ src/TL_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 class WeatherTLPipeline:
     def __init__(self):
@@ -7,3 +8,35 @@ class WeatherTLPipeline:
         self.db_user = os.getenv("DB_USER")
         self.db_password = os.getenv("DB_PASSWORD")
         self.db_port = os.getenv("DB_PORT")
+
+    
+    def get_raw_data(self):
+        try:
+            print("Підключаємось до бази даних...")
+            conn = psycopg2.connect(
+                host=self.db_host,
+                database=self.db_name,
+                user=self.db_user,
+                password=self.db_password,
+                port=self.db_port
+            )
+
+            cur = conn.cursor()
+            print("Успішно підключено до бази даних!")
+
+            sql_query_metadata = "SELECT last_processed_timestamp FROM metadata.pipeline_state WHERE pipeline_name = 'weather_transform'"
+            cur.execute(sql_query_metadata)
+            temp = cur.fetchone()
+            last_timestamp = temp[0]
+
+            sql_query_raw = "SELECT raw_id , raw_content , extracted_at FROM raw.weather_content WHERE extracted_at > %s"
+            cur.execute(sql_query_raw,(last_timestamp,))
+            raw_data = cur.fetchall()
+
+            cur.close()
+            conn.close()
+
+            return raw_data
+
+        except Exception as e:
+            print(f"Помилка при роботі з базою даних: {e}")
