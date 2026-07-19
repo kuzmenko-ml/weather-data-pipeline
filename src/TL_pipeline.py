031e245840f2b7760491bcc32f41329f6fa4b217
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-19 13:43:09 +0300

    feat(TL pipeline): init WeatherTLPipeline class and set up configuration

---
+++ src/TL_pipeline.py
@@ -0,0 +1,9 @@
+import os
+
+class WeatherTLPipeline:
+    def __init__(self):
+        self.db_host = os.getenv("DB_HOST")
+        self.db_name = os.getenv("DB_NAME")
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
+        self.db_port = os.getenv("DB_PORT")
