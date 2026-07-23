168ac3682197f8dfd1e527cd798ce988bbe81ff0
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-23 10:26:52 +0300

    refactor(EL): remove redundant .env variable loading in __init__

--- src/EL_pipeline.py
+++ src/EL_pipeline.py
@@ -10,11 +10,6 @@ load_dotenv()
 class WeatherELPipeline:
     def __init__(self):
         self.api_key = os.getenv("OPENWEATHER_API_KEY")
-        self.db_host = os.getenv("DB_HOST")
-        self.db_name = os.getenv("DB_NAME")
-        self.db_user = os.getenv("DB_USER")
-        self.db_password = os.getenv("DB_PASSWORD")
-        self.db_port = os.getenv("DB_PORT")
 
     def fetch_weather(self, city_name="Kyiv"):
         url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={self.api_key}&units=metric"
